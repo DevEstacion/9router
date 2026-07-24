bbf4c0b5026afbc542614fdee39fbb540b439557
ron <devestacion@gmail.com>
2026-07-23 22:04:56 -0700

    fix(codex): import response log formatter

--- open-sse/handlers/chatCore/sseToJsonHandler.js
+++ open-sse/handlers/chatCore/sseToJsonHandler.js
@@ -4,7 +4,7 @@ import { HTTP_STATUS } from "../../config/runtimeConfig.js";
 import { FORMATS } from "../../translator/formats.js";
 import { fromOpenAIFinish } from "../../translator/concerns/finishReason.js";
 import { PROVIDERS } from "../../config/providers.js";
-import { buildRequestDetail, extractRequestConfig, saveUsageStats } from "./requestDetail.js";
+import { buildRequestDetail, extractRequestConfig, saveUsageStats, formatDoneLine } from "./requestDetail.js";
 
 // Responses-API providers (e.g. codex) may emit SSE without content-type + use Responses output shape
 const isResponsesProvider = (p) => PROVIDERS[p]?.format === FORMATS.OPENAI_RESPONSES;
